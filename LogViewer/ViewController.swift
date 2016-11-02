3af6030a20d925ea7c4914b1c73f539619fd5ff8
Henrik Mygind <henm@vasosxdev19.widex.org>
2016-11-02 13:19:55 +0100

    Initial Commit

---
+++ LogViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LogViewer
+//
+//  Created by Henrik Mygind on 02/11/2016.
+//  Copyright © 2016 Widex A/S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
