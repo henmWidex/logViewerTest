092008680e48e582a1e5a538166cdcf89e1bd875
Henrik Mygind <henm@vasosxdev19.widex.org>
2016-11-08 12:22:24 +0100

    removed handler from Model

--- LogViewer/Model.swift
+++ LogViewer/Model.swift
@@ -14,15 +14,14 @@ class Model {
     
     let externalLib: ExternalLib;
     
-    var exampleList: [UInt32] = [1, 2, 1, 1, 0, 2, 0, 1, 2, 2, 0, 0];
-    
-    var handler: ((UInt32) -> Void)?
-    
     init() {
         self.externalLib = ExternalLib()
         self.externalLib.subscribe(callback: self.onLogReceived)
     }
     
+    
+    var exampleList: [UInt32] = [1, 2, 1, 1, 0, 2, 0, 1, 2, 2, 0, 0];
+
     func onLogReceived(value: UInt32){
         // TODO handle this        
     }
